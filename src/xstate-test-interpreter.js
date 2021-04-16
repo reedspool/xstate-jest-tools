cb1c6fe812dc526626234531d0bc2bc840c5e59d
Reed Spool <reedwith2es@gmail.com>
2021-04-16 09:27:16 -0600

    Change interpreter.transition to send, convert to ES6 Class, and add interpreter.state non-shorthand

--- src/xstate-test-interpreter.js
+++ src/xstate-test-interpreter.js
@@ -1,24 +1,36 @@
 // [[file:../literate/XStateTestInterpreter.org::*Implementation][Implementation:1]]
-export const TestInterpreter = (machine) => {
-    return new _TestInterpreter(machine);
-}
-
-function _TestInterpreter(machine) {
-    this.machine = machine;
-    this.S = machine.initialState;
-    this.C = this.S.context;
-}
-
-_TestInterpreter.prototype.transition = function (event) {
-    this.S = this.machine.transition(this.S, event);
-    this.C = this.S.context;
-
-    // Get the side-effect actions to execute
-    const { actions } = this.S;
-
-    actions.forEach(action => {
-        // If the action is executable, execute it
-        action.exec && action.exec();
-    });
+export const TestInterpreter = (...args) => {
+    return new _TestInterpreter(...args);
 }
 // Implementation:1 ends here
+
+// [[file:../literate/XStateTestInterpreter.org::*Implementation][Implementation:2]]
+class _TestInterpreter {
+// Implementation:2 ends here
+
+// [[file:../literate/XStateTestInterpreter.org::*Implementation][Implementation:3]]
+    constructor(machine) {
+        this.machine = machine;
+        this.state = machine.initialState;
+        this.S = this.state;
+        this.C = this.state.context;
+    }
+// Implementation:3 ends here
+
+// [[file:../literate/XStateTestInterpreter.org::*Implementation][Implementation:4]]
+    send (event) {
+        this.state = this.machine.transition(this.state, event);
+        this.S = this.state;
+        this.C = this.state.context;
+
+        // Get the side-effect actions to execute
+        this.state.actions.forEach(action => {
+            // If the action is executable, execute it
+            action.exec && action.exec();
+        });
+    }
+// Implementation:4 ends here
+
+// [[file:../literate/XStateTestInterpreter.org::*Implementation][Implementation:5]]
+}
+// Implementation:5 ends here
